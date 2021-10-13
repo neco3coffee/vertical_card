cadbe277cc71c8f7d2a5d7fbaebf70ea6014443b
neco3coffee <neco3coffee@gmail.com>
2021-10-13 15:01:21 +0900

    Add vertical scroll card, vertical_card_pager: ^1.5.0は現時点で動作確認済み

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 void main() {
   runApp(const MyApp());
@@ -61,55 +62,65 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+// ------------------------------------
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final List<String> titles = [
+      "RED",
+      "YELLOW",
+      "BLACK",
+      "CYAN",
+      "BLUE",
+      "GREY",
+    ];
+
+    final List<Widget> images = [
+      Container(
+        color: Colors.red,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      Container(
+        color: Colors.yellow,
+      ),
+      Container(
+        color: Colors.black,
+      ),
+      Container(
+        color: Colors.cyan,
+      ),
+      Container(
+        color: Colors.blue,
+      ),
+      Container(
+        color: Colors.grey,
+      ),
+    ];
+
+    return Scaffold(
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Container(
+                child: VerticalCardPager(
+                    titles: titles, // required
+                    images: images, // required
+                    textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold), // optional
+                    onPageChanged: (page) {
+                      // optional
+                    },
+                    onSelectedItem: (index) {
+                      // optional
+                    },
+                    initialPage: 0, // optional
+                    align: ALIGN.CENTER // optional
+                    ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
